9d03776177ad623fb4f599c3a624c1da178bb93f
Majid Rahimi <Majid88Rahimi@gmail.com>
2021-03-27 21:08:29 +0200

    Wrote header with JS

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,6 +1,70 @@
 let headerTag = document.getElementById('header')
 let footerTag = document.getElementById('footer')
 
+
+function header() {
+    const logoContainer = document.createElement('div')
+    logoContainer.setAttribute('class', 'logo__container')
+    const logoLink = document.createElement('a')
+    logoLink.setAttribute('href', './index.html')
+    const logoImg = document.createElement('img')
+    logoImg.setAttribute('src', 'wwwroot/image/Asset 1.png')
+    logoImg.setAttribute('alt', 'logo')
+    logoLink.appendChild(logoImg)
+    logoContainer.appendChild(logoLink)
+
+    const nav = document.createElement('nav')
+    nav.setAttribute('class', 'main-menu__container')
+    const ul = document.createElement('ul')
+    const liMenu = [
+        {href: './index.html', name: 'Home'},
+        {href: '#', name: 'ABOUT US'},
+        {href: './contact-us.html', name: 'CONTACT US'},
+        {href: './our_services.html', name: 'SERVICES'},
+        {href: '#', name: 'OUR PROJECTS'}
+    ]
+    for (let index = 0; index < liMenu.length; index++) {
+        let li = document.createElement('li')
+        let a = document.createElement('a')
+        a.setAttribute('href', liMenu[index].href)
+        let name = document.createTextNode(liMenu[index].name)
+        a.appendChild(name)
+        li.appendChild(a)
+        ul.appendChild(li)
+    }
+
+    const ham = document.createElement('img')
+    const hamSrc = './wwwroot/image/ham.png'
+    const timesSrc = './wwwroot/image/times.png'
+    ham.setAttribute('src', hamSrc)
+    ham.setAttribute('class', 'ham')
+    ham.setAttribute('alt', 'ham')
+    ham.addEventListener('click', () => {
+        if (ham.alt === 'ham') {
+            ham.setAttribute('src', timesSrc)
+            ham.setAttribute('alt', 'times')
+            headerTag.setAttribute('id', 'mobile-menu-header')
+        } else {
+            ham.setAttribute('src', hamSrc)
+            ham.setAttribute('alt', 'ham')
+            headerTag.setAttribute('id', '')
+        }
+    })
+    window.addEventListener('resize', function() {
+        if (window.screen.width > 600) {
+            headerTag.setAttribute('id', '')
+            ham.setAttribute('src', hamSrc)
+        }
+    })
+
+    nav.append(ul, ham)
+
+    headerTag.append(logoContainer, nav)
+}
+header()
+
+
+
 function footer() {
     return (`
         <div class="image">
@@ -23,48 +87,4 @@ function footer() {
         </div>    
     `)
 }
-
-function header() {
-    return (`
-        <div class="logo__container">
-            <a href="./index.html"><img src="wwwroot/image/Asset 1.png" alt="Logo"></a>
-        </div>
-        <nav class="main-menu__container">
-            <ul>
-                <li><a href="./index.html">HOME</a></li>
-                <li>ABOUT US</li>
-                <li><a href="./contact-us.html">CONTACT US</a></li>
-                <li><a href="./our_services.html">SERVICES</a></li>
-                <li>OUR PROJECTS</li>
-            </ul>
-            <img onclick="handleHamClick()" class="ham" src="./wwwroot/image/ham.png" alt="ham">
-        </nav>
-    `)
-}
-
-headerTag.innerHTML = header()
 footerTag.innerHTML = footer()
-
-let ham = document.getElementsByClassName("ham")[0]
-
-function handleHamClick() {
-
-    let length = ham.src.length
-    let match = ham.src.slice(length - 7, length - 4)
-
-    if (match == "ham") {
-        ham.src = "./wwwroot/image/times.png"
-        // menu.style.display = "flex"
-        headerTag.setAttribute('id', 'mobile-menu-header')
-    } else {
-        ham.src = "./wwwroot/image/ham.png"
-        headerTag.setAttribute('id', '')
-    }
-}
-
-window.addEventListener('resize', function() {
-    if (window.screen.width > 600) {
-        headerTag.setAttribute('id', '')
-        ham.src = "./wwwroot/image/ham.png"
-    }
-})

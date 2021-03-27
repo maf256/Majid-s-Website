7dae9e2aeec6eb74c4433b630633e715cf6fb53c
Majid Rahimi <Majid88Rahimi@gmail.com>
2021-03-27 17:34:15 +0200

    Made header and footer dynamic

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -1,5 +1,52 @@
-let header = document.getElementsByTagName('header')[0]
+let headerTag = document.getElementById('header')
+let footerTag = document.getElementById('footer')
+
+function footer() {
+    return (`
+        <div class="image">
+            <img src="wwwroot/image/Asset 1.png" alt="Logo">
+        </div>      
+        <div class="apps__footer">
+            <h1>CONTACT US</h1>
+            <ul class="social__logo">
+                <li><img src="wwwroot/image/Asset 1.png" alt=""></li>
+                <li><img src="wwwroot/image/Asset 1.png" alt=""></li>
+                <li><img src="wwwroot/image/Asset 1.png" alt=""></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="menu">
+                <li>ABOUT US</li>
+                <li>OUR SERVICES</li>
+                <li>OUR PROJECT</li>
+            </ul>
+        </div>    
+    `)
+}
+
+function header() {
+    return (`
+        <div class="logo__container">
+            <a href="./index.html"><img src="wwwroot/image/Asset 1.png" alt="Logo"></a>
+        </div>
+        <nav class="main-menu__container">
+            <ul>
+                <li><a href="./index.html">HOME</a></li>
+                <li>ABOUT US</li>
+                <li><a href="./contact-us.html">CONTACT US</a></li>
+                <li><a href="./our_services.html">SERVICES</a></li>
+                <li>OUR PROJECTS</li>
+            </ul>
+            <img onclick="handleHamClick()" class="ham" src="./wwwroot/image/ham.png" alt="ham">
+        </nav>
+    `)
+}
+
+headerTag.innerHTML = header()
+footerTag.innerHTML = footer()
+
 let ham = document.getElementsByClassName("ham")[0]
+
 function handleHamClick() {
 
     let length = ham.src.length
@@ -8,16 +55,16 @@ function handleHamClick() {
     if (match == "ham") {
         ham.src = "./wwwroot/image/times.png"
         // menu.style.display = "flex"
-        header.setAttribute('id', 'mobile-menu-header')
+        headerTag.setAttribute('id', 'mobile-menu-header')
     } else {
         ham.src = "./wwwroot/image/ham.png"
-        header.setAttribute('id', '')
+        headerTag.setAttribute('id', '')
     }
 }
 
 window.addEventListener('resize', function() {
     if (window.screen.width > 600) {
-        header.setAttribute('id', '')
+        headerTag.setAttribute('id', '')
         ham.src = "./wwwroot/image/ham.png"
     }
 })
